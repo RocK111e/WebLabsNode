87590e33c6941a0888aa2c3092aed85baceb6be6
RocK111e <romanmeg9@gmail.com>
2025-05-05 11:13:51 +0300

    node setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end('Hello, Node.js Backend with pnpm!');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end('Not Found');
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
